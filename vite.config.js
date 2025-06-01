eda3fda3c19c386d70fdf35c5d55f5fab6fd6296
Rahesa-Aferdinan <rahesaaferdinand87@gmail.com>
2025-06-01 13:45:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
                 'resources/views/themes/indotoko/assets/plugins/jqueryui/jquery-ui.min.js',
 
 
-                'resources/views/livewire/admin/assets/css/tabler.min.css?1692870487',
-                'resources/views/livewire/admin/assets/css/tabler-flags.min.css?1692870487',
-                'resources/views/livewire/admin/assets/css/tabler-payments.min.css?1692870487',
-                'resources/views/livewire/admin/assets/css/tabler-vendors.min.css?1692870487',
+                'resources/views/livewire/admin/assets/css/tabler.min.css',
+                'resources/views/livewire/admin/assets/css/tabler-flags.min.css',
+                'resources/views/livewire/admin/assets/css/tabler-payments.min.css',
+                'resources/views/livewire/admin/assets/css/tabler-vendors.min.css',
 
-                'resources/views/livewire/admin/assets/js/demo-theme.min.js?1692870487',
+                'resources/views/livewire/admin/assets/js/demo-theme.min.js',
                 'resources/views/livewire/admin/assets/libs/apexcharts/dist/apexcharts.min.js',
                 'resources/views/livewire/admin/assets/libs/jsvectormap/dist/js/jsvectormap.min.js',
                 'resources/views/livewire/admin/assets/libs/jsvectormap/dist/maps/world.js',
